904d4d4a6768a6cbf20b525ce60affef0de3b978
verstak <verstak2020@gmail.com>
2020-12-17 08:45:07 -0500

    commit maybe the end

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         dots: true,
         responsive: [
             {
-                breakpoint: 400,
+                breakpoint: 767,
                 settings: "unslick",
             },
             {
@@ -14,7 +14,6 @@ $(document).ready(function () {
                 }
             }
         ]
-
     });
     $('.burger').on('click', function () {
         $(this).toggleClass('active');
@@ -23,4 +22,44 @@ $(document).ready(function () {
     $('.show').on('click', function () {
         $(this).toggleClass('active').siblings('.sublist').slideToggle();
     })
+});
+
+$(document).ready(function () {
+    $('.toggle').on('click', function () {
+        $(this).toggleClass('active');
+        $(this).next('.main-catalog-filter__sublist').slideToggle();
+    });
+    $('.toggle').on('click', function () {
+        $(this).next('.main-catalog-filter__subsublist').slideToggle();
+    })
 })
+
+$(document).ready(function () {
+    $('.main-catalog-filter__title-planshet').on('click', function () {
+        $('.main-catalog-filter__main-list').toggleClass('active');
+    })
+    $('.main-catalog-filter__cross').on('click', function () {
+        $('.main-catalog-filter__main-list').removeClass('active');
+    })
+})
+
+$(document).ready(function () {
+    $('.contact-us').on('click', function () {
+        $('#modal').toggleClass('active');
+    })
+    $('.modal__inner .cross').on('click', function () {
+        $('#modal').removeClass('active');
+    })
+})
+
+$(document).mouseup(function (e) {
+    var div = $(".modal__wrapper");
+    var toggle = $('.contact-us');
+    if (!div.is(e.target)
+        &&
+        !toggle.is(e.target)
+        &&
+        div.has(e.target).length === 0) {
+        $("#modal").removeClass('active');
+    }
+});
